0141b43dea390650f32781224392e4ef195d75a4
Your Name <you@example.com>
2021-03-05 17:15:04 -0300

    1.0.10

--- bin/module.js
+++ bin/module.js
@@ -12,15 +12,15 @@ shell(`mkdir -p src/pages`);
 shell(`cp -r node_modules/think-react-form/lib/modules src `);
 
 //CHECK PACKAGES DEPENDENTS
-shell(`[ -d "node_modules/react-input-mask" ] || npm install react-input-mask`);
-shell(`[ -d "node_modules/react-hook-form" ] || npm install react-hook-form`);
-shell(`[ -d "node_modules/yup" ] || npm install yup`);
-shell(`[ -d "node_modules/@hookform/resolvers" ] || npm install @hookform/resolvers`);
-shell(`[ -d "node_modules/@material-ui/core" ] || npm install @material-ui/core`);
-shell(`[ -d "node_modules/@material-ui/icons" ] || npm install @material-ui/icons`);
-shell(`[ -d "node_modules/@material-ui/lab" ] || npm install @material-ui/lab`);
-shell(`[ -d "node_modules/axios" ] || npm install axios`);
-shell(`[ -d "node_modules/moment" ] || npm install moment`);
+shell(`[ -d "node_modules/react-input-mask" ] || npm install react-input-mask@2.0.4`);
+shell(`[ -d "node_modules/react-hook-form" ] || npm install react-hook-form@6.15.4`);
+shell(`[ -d "node_modules/yup" ] || npm install yup@0.28.1`);
+shell(`[ -d "node_modules/@hookform/resolvers" ] || npm install @hookform/resolvers@1.3.5`);
+shell(`[ -d "node_modules/@material-ui/core" ] || npm install @material-ui/core@4.9.5`);
+shell(`[ -d "node_modules/@material-ui/icons" ] || npm install @material-ui/icons@4.5.1`);
+shell(`[ -d "node_modules/@material-ui/lab" ] || npm install @material-ui/lab@4.0.0-alpha.40`);
+shell(`[ -d "node_modules/axios" ] || npm install axios@0.21.1`);
+shell(`[ -d "node_modules/moment" ] || npm install moment@2.29.1`);
 shell(`[ -d "node_modules/react-router-dom" ] || npm install react-router-dom`);
 
 //CHECK AND CREATE DIRECTORY MODEL
